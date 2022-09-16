1671ab8f0a06baf3bd3c3de9bf21eb000ecebcdc
TikkiTikki <radstylemusic@gmail.com>
2022-09-16 19:14:12 -0300

    Initial load

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require('socket.io');
+const io = new Server(server, {
+  cors: {
+    origin: '*',
+  },
+});
+
+app.get('/', (req, res) => {
+  res.send('<h1>Socket server</h1>');
+});
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+  const createdAlert = (msg) => {
+    socket.broadcast.emit('newIncomingAlert', msg);
+  };
+
+  socket.on('createdAlert', createdAlert);
+});
+
+server.listen(3000, () => {
+  console.log('listening on *:3000');
+});
