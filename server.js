9b2ceff46aa3a2bf06c39b4c821e65b1e39b52a9
TikkiTikki <radstylemusic@gmail.com>
2022-11-20 22:02:02 -0300

    Add logger and Medical 57 Sockets

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const app = express();
 const http = require('http');
 const server = http.createServer(app);
 const { Server } = require('socket.io');
+const logger = require('./log');
 const io = new Server(server, {
   cors: {
     origin: '*',
@@ -14,20 +15,36 @@ app.get('/', (req, res) => {
 });
 
 io.on('connection', (socket) => {
-  console.log('a user connected');
+  logger.debug('a user connected');
   const createdAlert = (msg) => {
     socket.broadcast.emit('newIncomingAlert', msg);
+    logger.debug('emiting newIncomingAlert msg');
   };
 
   socket.on('createdAlert', createdAlert);
 
   const createdLocation = (msg) => {
     socket.broadcast.emit('newIncomingLocation', msg);
+    logger.debug('emiting newIncomingLocation msg');
   };
 
   socket.on('createdLocation', createdLocation);
+
+  const createdBlueCodeAlert = (msg) => {
+    socket.broadcast.emit('newIncomingBlueCodeAlert', msg);
+    logger.debug('emiting newIncomingBlueCodeAlert msg');
+  };
+
+  socket.on('createdBlueCodeAlert', createdBlueCodeAlert);
+
+  const createdConfirmNurse = (msg) => {
+    socket.broadcast.emit('newIncomingConfirmNurse', msg);
+    logger.debug('emiting newIncomingConfirmNurse msg');
+  };
+
+  socket.on('createdConfirmNurse', createdConfirmNurse);
 });
 
 server.listen(3000, () => {
-  console.log('listening on *:3000');
+  logger.debug('listening on *:3000');
 });
